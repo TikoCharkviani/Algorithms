ef054d2ee0e8d22d8444b2a10e8e9a0dd0fbc3e4
Tiko Charkviani <tikocharkviani@gmail.com>
2023-06-29 20:33:13 +0400

    Noldbach problem

--- C++/Noldbach.cpp
+++ C++/Noldbach.cpp
@@ -2,43 +2,30 @@
 #include <vector>
 using namespace std;
 
-const int MAXN = 1000;
-bool num[MAXN]; 
-vector<int> primes;
-
-void generatePrimes(int n) {
-    int i,j;
-    for(i=2; i <= MAXN; i++) {
-        if (num[i] == 1) {
-            continue;
-        } else {
-            primes.push_back(i);
-            for(j = i*i; j<=n; j = j+i) {
-                num[j] = 1;
-            }
-        }
-    }
-}
-
-
-bool check_condition(int n, int k) {
-    generatePrimes(n);
-    int cnt = 0;
-    for (int i = 2; i < primes.size(); ++i) {
-        if (primes[i] == primes[i - 1] + primes[i - 2] + 1 && primes[i] <= n) {
-            ++cnt;
-        }
-    }
-    return cnt >= k;
-}
-
 int main() {
     int n, k;
     cin >> n >> k;
-    if (check_condition(n, k)) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
+
+    bool prime[n+1]; 
+    memset(prime, true, sizeof(prime)); 
+
+    for (int p=2; p*p<=n; p++) {
+        if (prime[p] == true) { 
+            for (int i=p*p; i<=n; i += p) 
+                prime[i] = false; 
+        }
     }
+
+    int count = 0;
+    for (int p=2; p<=n; p++) {
+        if (prime[p] && prime[p+2]) 
+            count++;
+    }
+
+    if (count >= k) 
+        cout << "YES"; 
+    else
+        cout << "NO";
+
     return 0;
 }
